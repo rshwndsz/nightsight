a6815c50bed1d26aedffa5094eb7ed79eb6888b3
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-02-28 22:58:15 +0530

    Add one step install to a script

---
+++ scripts/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+python3 -m venv ./env
+source ./env/bin/activate
+pip3 install --upgrade pip
+
+if [[ $1 -eq "training" ]]; then
+    pip install -e .[training]
+else
+    pip install -e .
+fi
