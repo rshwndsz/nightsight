cff7e8052606c11fa9feee94c3fb14272e709fc9
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-02-28 12:27:07 +0530

    Add tests for losses

---
+++ nightsight/loss_test.py
@@ -0,0 +1,43 @@
+import torch
+import pytest
+from nightsight import loss
+
+
+def test_LSpaLoss():
+    inputs = torch.rand(4, 3, 256, 256)
+    enhanced_images = torch.rand(4, 3, 256, 256)
+    L_spa = loss.LSpaLoss(type_ref=inputs)
+    loss_spa = torch.mean(L_spa(enhanced_images, inputs))
+    assert (not torch.isnan(loss_spa))
+
+
+def test_LExpLoss():
+    enhanced_images = torch.rand(4, 3, 256, 256)
+    L_exp = loss.LExpLoss(16, 0.6)
+    loss_exp = torch.mean(L_exp(enhanced_images))
+    assert (not torch.isnan(loss_exp))
+
+
+def test_LColorLoss():
+    enhanced_images = torch.rand(4, 3, 256, 256)
+    L_color = loss.LColorLoss()
+    loss_col = torch.mean(L_color(enhanced_images))
+    assert (not torch.isnan(loss_col))
+
+
+@pytest.mark.skip(reason="WIP")
+def test_LTVLoss():
+    A = torch.rand(24, 3, 256, 256)
+    L_tv = loss.LTVLoss()
+    loss_tv = L_tv(A)
+    assert (not torch.isnan(loss_col))
+
+
+@pytest.mark.skip(reason="Not used")
+def test_SaLoss():
+    pass
+
+
+@pytest.mark.skip(reason="Not used")
+def test_PerceptionLoss():
+    pass
