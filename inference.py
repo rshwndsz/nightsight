efc5be4e9b68a454f5fecc41a87b4bcf54d85e9f
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-03-04 09:56:39 +0530

    Add more logs and cli args

--- inference.py
+++ inference.py
@@ -1,15 +1,15 @@
 import argparse
-import torch
+from pathlib import Path
+import logging
+
 import numpy as np
 from PIL import Image
+import torch
 from torchvision.utils import save_image
-from pathlib import Path
 
 from nightsight import model
 from nightsight.log import Logger
 logger = Logger()
-
-IMAGE_SIZE = (256, 256)
 
 
 def inference(args):
@@ -25,9 +25,10 @@ def inference(args):
     for image_path in args.images:
         # TODO Split image into patches, compute enhaced parallely and average
         # Load
+        logger.debug(f"Loading {image_path}")
         image = Image.open(image_path)
         # Resize
-        image = image.resize(IMAGE_SIZE)
+        image = image.resize((args.size, args.size))
         # Tensorify and convert to [C H W]
         image = torch.from_numpy(np.array(image)).permute(2, 0, 1)
         # Normalize
@@ -42,13 +43,21 @@ def inference(args):
         # ei = torch.clamp(ei * torch.Tensor([255]), 0, 255)
         logger.debug(f"ei: {ei.min()} -> {ei.max()}")
         # Save
-        save_image(ei, Path(args.outdir) / image_path.split('/')[-1])
+        Path(args.outdir).mkdir(parents=True, exist_ok=True)
+        output_name = Path(args.outdir) / (image_path.split('/')[-1])
+        logger.debug(f"Saving {output_name}")
+        save_image(ei, output_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-w", "--weights", required=True)
+    parser.add_argument("-s", "--size", type=int, default=256)
     parser.add_argument("-i", "--images", nargs='+', required=True)
     parser.add_argument("-o", "--outdir", default="./data/output/")
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true")
+    parser.set_defaults(verbose=False)
     args = parser.parse_args()
+    if not args.verbose:
+        logger.logger.setLevel(logging.INFO)
     inference(args)
