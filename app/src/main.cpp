f62145ca19b71f117a9da7d72896fecd8c36befe
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-03-05 13:40:09 +0530

    Get C++-based enhancement running
    * Comment out the resize part for now
    * Add helpful logging of steps
    * Add error-checks
    * Correct loading, enhancement and writing images (Proper dtypes)

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,64 +1,139 @@
 // Inference.cpp
-// TODO Description
-
-// User includes
 #include "config.h"
-#include <torch/script.h>
-
-#include <stdlib.h>
-
 #include <iostream>
 #include <memory>
+#include <stdlib.h>
+#include <torch/script.h>
 #include <tuple>
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_resize.h"
+#include "stb_image_write.h"
 
 int main(int argc, const char *argv[]) {
   // Check usage
   if (argc != 3) {
     // Report version
     std::cout << argv[0] << " Version " << Nightsight_VERSION_MAJOR << "."
-                 << Nightsight_VERSION_MINOR << std::endl;
+              << Nightsight_VERSION_MINOR << std::endl;
+    // Report usage
     std::cerr << "Usage: " << argv[0]
               << " <path-to-exported-script-module> <path-to-original-image>\n";
     exit(EXIT_FAILURE);
   }
 
   // Load Model
+  std::cout << "Loading model";
   torch::jit::script::Module module;
   try {
     // Deserialize the ScriptModule from a file using torch::jit::load()
     module = torch::jit::load(argv[1]);
   } catch (const c10::Error &e) {
-    std::cerr << "Error loading the model \n";
+    std::cerr << " - failed\n";
     exit(EXIT_FAILURE);
   }
-  // TODO Move this to logging
-  std::cout << "Loaded model successfully\n";
-
-  // Create a Vector of inputs and add a single input
-  // `torch::jit::Ivalue` (a type-erased value type `script::Module` methods
-  // accept and return)
-  std::vector<torch::jit::IValue> inputs;
-  inputs.push_back(torch::ones({1, 3, 256, 256}));
+  std::cout << " - done\n";
 
   // Load image
-  // See: https://stackoverflow.com/a/40812978
-  // int width, height, bpp;
-  // uint8_t *image = stbi_load(argv[2], &width, &height, &bpp, 3);
-  // stbi_image_free(image);
+  // https://stackoverflow.com/a/40812978
+  int width, height, ch;
+  unsigned char *image;
+  std::cout << "Loading model";
+  image = stbi_load(/*filename*/ argv[2],
+                    /*input_width*/ &width,
+                    /*input_height*/ &height,
+                    /*8-bit components per-pixel*/ &ch,
+                    /*desired_channels*/ 0);
+  if (!image) {
+    std::cerr << " - failed\n";
+    exit(EXIT_FAILURE);
+  }
+  std::cout << " - done\n";
 
   // Resize image
+  // std::cout << "Allocating memory for resized image";
+  // unsigned char *resized;
+  // resized = (unsigned char *)malloc(256 * 256 * ch);
+  // if (!resized) {
+  //   std::cout << " - failed\n";
+  //   exit(EXIT_FAILURE);
+  // }
+  // std::cout << " - done\n";
+  // std::cout << "Resizing image";
+  // int resize_status = stbir_resize_uint8(/*input_data*/ image,
+  //                                        /*input_width*/ width,
+  //                                        /*input_height*/ height,
+  //                                        /*input_stride_in_bytes*/ 0,
+  //                                        /*ouput_data*/ resized,
+  //                                        /*output_width*/ 256,
+  //                                        /*output_height*/ 256,
+  //                                        /*output_stride_in_bytes*/ 0,
+  //                                        /*NUM_CHANNELS*/ ch);
+  // if (!resize_status) {
+  //   std::cerr << " - failed\n";
+  //   exit(EXIT_FAILURE);
+  // } else {
+  //   std::cout << " - done\n";
+  // }
 
   // Convert image to Tensor
+  // https://stackoverflow.com/a/63154900
+  // https://github.com/pytorch/pytorch/issues/12506
+  std::cout << "Converting input to tensor";
+  torch::Tensor imageTensor = torch::from_blob(/*Pure c++ matrix data*/ image,
+                                               /*dims*/ {width, height, ch},
+                                               /*dtype*/ torch::kUInt8)
+                                  .clone()
+                                  .to(torch::kFloat32)
+                                  .clamp_(0.0, 255.0)
+                                  .div_(255.0)
+                                  .permute({2, 0, 1})
+                                  .unsqueeze(0);
+  std::cout << " - done ";
 
+  // Create a Vector of inputs
+  std::cout << "Enhancing input";
+  std::vector<torch::jit::IValue> inputs;
+  inputs.push_back(imageTensor);
   // Execute the model and turn its output into a tensor
-  auto output = module.forward(inputs).toTuple()->elements()[1].toTensor();
+  auto outputTensor = module.forward(inputs)
+                          .toTuple()
+                          ->elements()[1]
+                          .toTensor()
+                          .permute({0, 2, 3, 1})
+                          .mul_(255.0)
+                          .add_(0.5)
+                          .clamp_(0.0, 255.0)
+                          .to(torch::kUInt8);
+  std::cout << " - done\n";
 
   // Convert Tensor to image
+  std::cout << "Converting output tensor to data_ptr";
+  auto output = (unsigned char *)outputTensor.data_ptr();
+  if (!output) {
+    std::cout << " - failed\n";
+    exit(EXIT_FAILURE);
+  } else {
+    std::cout << " - done\n";
+  }
 
   // Write image
+  std::cout << "Writing output to file";
   // See: https://stackoverflow.com/a/40812978
-  // stbi_write_jpg("output.jpg", width, height, CHANNEL_NUM, image,
-  // width*CHANNEL_NUM);
+  int write_status = stbi_write_jpg(/*filename*/ "output.jpg",
+                                    /*width*/ width,
+                                    /*height*/ height,
+                                    /*NUM_CHANNELS*/ 3,
+                                    /*Matrix*/ output,
+                                    /*Quality*/ width * 3);
+  if (!write_status) {
+    std::cerr << " - failed\n";
+    exit(EXIT_FAILURE);
+  } else {
+    std::cout << " - done\n";
+  }
 
   exit(EXIT_SUCCESS);
 }
