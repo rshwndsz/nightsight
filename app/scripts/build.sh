be4238446def51c6f9804c217271e7a5150e506e
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-03-06 12:25:21 +0530

    Split build-sys generation and compilation

--- app/scripts/build.sh
+++ app/scripts/build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-rm -rf build
-mkdir build && cd build
-cmake ..
-cmake --build . --config Release
-make
-cd ..
+# Check if executing from the right place
+if [ ! -f $PWD/CMakeLists.txt ]; then
+    echo "Get into nightsight/app and then execute this script."
+    exit
+fi
 
+# Configure build sytem if not done already
+if [ ! -d $PWD/build ]; then
+    ./scripts/configure.sh
+fi
+
+# Build
+cd build
+cmake --build . --config Release
+cd ..
